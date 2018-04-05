1743ec77e7294ce02bbb25bb8afc1375bd8b3814
Federico Gimenez <federico@giantswarm.io>
2018-04-05 11:43:38 +0200

    remove cert-operator specific constants

--- pkg/framework/host.go
+++ pkg/framework/host.go
@@ -31,36 +31,6 @@ type PatchSpec struct {
 	Value interface{} `json:"value"`
 }
 
-const (
-	certOperatorValuesFile = "/tmp/cert-operator-values.yaml"
-	// certOperatorChartValues values required by cert-operator-chart, the environment
-	// variables will be expanded before writing the contents to a file.
-	certOperatorChartValues = `commonDomain: ${COMMON_DOMAIN_GUEST}
-clusterName: ${CLUSTER_NAME}
-Installation:
-  V1:
-    Auth:
-      Vault:
-        Address: http://vault.default.svc.cluster.local:8200
-        CA:
-          TTL: 1440h
-    Guest:
-      Kubernetes:
-        API:
-          EndpointBase: ${COMMON_DOMAIN_GUEST}
-    Secret:
-      CertOperator:
-        SecretYaml: |
-          service:
-            vault:
-              config:
-                token: ${VAULT_TOKEN}
-      Registry:
-        PullSecret:
-          DockerConfigJSON: "{\"auths\":{\"quay.io\":{\"auth\":\"$REGISTRY_PULL_SECRET\"}}}"
-`
-)
-
 type HostConfig struct {
 	Backoff *backoff.ExponentialBackOff
 }
