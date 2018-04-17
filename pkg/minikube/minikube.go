5645453ee51492fbcfe24d30d1e915e494d64978
Federico Gimenez <federico@giantswarm.io>
2018-04-17 16:06:44 +0200

    show build errors

--- pkg/minikube/minikube.go
+++ pkg/minikube/minikube.go
@@ -84,7 +84,7 @@ func (m *Minikube) getDockerEnv() ([]string, error) {
 
 func (m *Minikube) buildImage(binaryName, path, imageName string, env []string) error {
 	if err := m.builder.Build(ioutil.Discard, imageName, path, m.imageTag, env); err != nil {
-		fmt.Println("error building image", imageName)
+		m.logger.Log("error", fmt.Sprintf("could not build image %s: %v", imageName, err))
 		return microerror.Mask(err)
 	}
 	return nil
