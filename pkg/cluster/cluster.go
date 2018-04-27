db09a7dc1f4b3a3c1999e076969112b2bf89ab95
Paweł Kopiczko <kopiczko@gmail.com>
2018-04-27 09:39:49 +0200

    cluster, cmd: don't copy tty on setup (#68)

--- pkg/cluster/cluster.go
+++ pkg/cluster/cluster.go
@@ -87,6 +87,12 @@ func (c *Cluster) copyMinikubeAssets(homeDir string) error {
 
 	// copy minikube directory
 	walkFn := func(path string, info os.FileInfo, err error) error {
+		if strings.HasSuffix(path, "/tty") {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
 		targetPath := strings.Replace(path, originDir, targetDir, 1)
 		if info.IsDir() {
 			return c.fs.MkdirAll(targetPath, os.ModePerm)
