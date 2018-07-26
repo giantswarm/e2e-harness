eaab26fe4f5aeadc268242169bdc93838da2d424
Ross Fairbanks <ross@giantswarm.io>
2018-07-26 13:52:40 +0200

    Add WaitForStatus and WaitForVersion to resource package

--- pkg/framework/resource/resource.go
+++ pkg/framework/resource/resource.go
@@ -2,7 +2,9 @@ package resource
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"time"
 
 	"github.com/cenkalti/backoff"
 	"github.com/giantswarm/apprclient"
@@ -91,5 +93,53 @@ func (r *Resource) InstallResource(name, values, channel string, conditions ...f
 		}
 	}
 
+	return nil
+}
+
+func (r *Resource) WaitForStatus(gsHelmClient *helmclient.Client, release string, status string) error {
+	operation := func() error {
+		rc, err := r.helmClient.GetReleaseContent(release)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+		if rc.Status != status {
+			return microerror.Maskf(releaseStatusNotMatchingError, "waiting for %q, current %q", status, rc.Status)
+		}
+		return nil
+	}
+
+	notify := func(err error, t time.Duration) {
+		log.Printf("getting release status %s: %v", t, err)
+	}
+
+	b := framework.NewExponentialBackoff(framework.ShortMaxWait, framework.LongMaxInterval)
+	err := backoff.RetryNotify(operation, b, notify)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+	return nil
+}
+
+func (r *Resource) WaitForVersion(gsHelmClient *helmclient.Client, release string, version string) error {
+	operation := func() error {
+		rh, err := gsHelmClient.GetReleaseHistory(release)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+		if rh.Version != version {
+			return microerror.Maskf(releaseVersionNotMatchingError, "waiting for %q, current %q", version, rh.Version)
+		}
+		return nil
+	}
+
+	notify := func(err error, t time.Duration) {
+		log.Printf("getting release version %s: %v", t, err)
+	}
+
+	b := framework.NewExponentialBackoff(framework.ShortMaxWait, framework.LongMaxInterval)
+	err := backoff.RetryNotify(operation, b, notify)
+	if err != nil {
+		return microerror.Mask(err)
+	}
 	return nil
 }
