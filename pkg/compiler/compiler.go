16e55b69d6b733b9cb0b49eae43987060c7f3674
Paweł Kopiczko <kopiczko@gmail.com>
2018-11-23 09:44:07 +0000

    cmd, pkg: use dockerized go 1.11.1 for compiling binaries (#167)
    Towards https://github.com/giantswarm/giantswarm/issues/4753.
    
    Before that e2e-harness was using OS go binary. On circleci this means
    go 1.9. This means client-go@1.12.2 won't compile. It could have also
    other side effects.

--- pkg/compiler/compiler.go
+++ pkg/compiler/compiler.go
@@ -2,14 +2,16 @@ package compiler
 
 import (
 	"context"
+	"fmt"
 	"os"
-	"os/exec"
 	"path/filepath"
+	"strings"
 
 	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/micrologger"
 
 	"github.com/giantswarm/e2e-harness/pkg/harness"
+	"github.com/giantswarm/e2e-harness/pkg/internal/golang"
 )
 
 type Config struct {
@@ -36,6 +38,10 @@ func New(config Config) *Compiler {
 
 // CompileMain is a Task that builds the main binary.
 func (c *Compiler) CompileMain(ctx context.Context) error {
+	binaryPath := harness.GetProjectName()
+
+	c.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("compiling binary %#q", binaryPath))
+
 	dir, err := os.Getwd()
 	if err != nil {
 		return microerror.Mask(err)
@@ -44,85 +50,36 @@ func (c *Compiler) CompileMain(ctx context.Context) error {
 	mainPath := filepath.Join(dir, "main.go")
 	_, err = os.Stat(mainPath)
 	if os.IsNotExist(err) {
-		c.logger.Log("level", "info", "message", "no main.go, skipping binary build")
+		c.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("did not compile binary %#q", binaryPath))
+		c.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("file main.go was not found"))
 		return nil
 	}
 
-	name := harness.GetProjectName()
-
-	c.logger.Log("level", "info", "message", "compiling binary "+name)
-	if err := c.compileMain(name, dir); err != nil {
+	err = golang.Go(ctx, "build", "-o", binaryPath, ".")
+	if err != nil {
 		return microerror.Mask(err)
 	}
 
+	c.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("compiled binary %#q", binaryPath))
 	return nil
 }
 
 // CompileTests is a Task that builds the tests binary.
 func (c *Compiler) CompileTests(ctx context.Context) error {
-	dir, err := os.Getwd()
+	binaryPath := filepath.Join(c.testDir, harness.GetProjectName()+"-e2e")
+
+	pkg := c.testDir
+	if !strings.HasPrefix(c.testDir, string(filepath.Separator)) {
+		pkg = "./" + pkg
+	}
+
+	c.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("compiling test binary %#q for package %#q", binaryPath, pkg))
+
+	err := golang.Go(ctx, "test", "-c", "-o", binaryPath, "-tags", "k8srequired", pkg)
 	if err != nil {
 		return microerror.Mask(err)
 	}
 
-	e2eBinary := harness.GetProjectName() + "-e2e"
-	e2eDir := filepath.Join(dir, c.testDir)
-
-	c.logger.Log("level", "info", "message", "compiling binary "+e2eBinary)
-	err = c.compileTests(e2eBinary, e2eDir)
-	if err != nil {
-		return microerror.Mask(err)
-	}
-
+	c.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("compiled test binary %#q for package %#q", binaryPath, pkg))
 	return nil
-}
-
-// compileMain compiles a go binary in the given path giving it the provided
-// name. If the binary already exists and is executable the build is skipped
-func (c *Compiler) compileMain(binaryName, path string) error {
-	// do not build if binary is already there
-	binPath := filepath.Join(path, binaryName)
-	if executebleExists(binPath) {
-		c.logger.Log("level", "info", "message", "main binary exists, not building")
-		return nil
-	}
-
-	cmd := exec.Command("go", "build", "-o", binaryName, ".")
-	cmd.Env = append(os.Environ(), "CGO_ENABLED=0", "GOOS=linux")
-	cmd.Dir = path
-
-	return cmd.Run()
-}
-
-// compileTests compiles a go test binary in the given path giving it the
-// provided name. If the binary already exists and is executable the build
-// is skipped
-func (c *Compiler) compileTests(binaryName, path string) error {
-	cmd := exec.Command("go", "test", "-c", "-o", binaryName, "-tags", "k8srequired", ".")
-	cmd.Env = append(os.Environ(), "CGO_ENABLED=0", "GOOS=linux")
-	cmd.Dir = path
-	cmd.Stderr = os.Stdout
-	cmd.Stdout = os.Stdout
-
-	return cmd.Run()
-}
-
-func executebleExists(path string) bool {
-	fi, err := os.Stat(path)
-	if err != nil {
-		return false
-	}
-
-	if fi.IsDir() {
-		return false
-	}
-
-	// 0111 octal represents a mode with the executable bit set
-	// for user, group and others. Performing a bitwise and with
-	// the mode of the file would only result in 0 if all these
-	// bit flags are 0, so if the result is different from 0 we
-	// can assume that the file is executable.
-	isExecutable := fi.Mode()&os.FileMode(0111) != 0
-
-	return isExecutable
 }
